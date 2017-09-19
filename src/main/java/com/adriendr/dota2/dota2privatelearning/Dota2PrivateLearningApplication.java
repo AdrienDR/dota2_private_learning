6d7caad2a5b8344475015fcca435b91e840a088c
arval4.prestation <arval4.prestation@ouest-france.fr>
2017-09-19 13:49:13 +0200

    Base de démarrage projet : Projet Spring boot avec configuratin minimale

---
+++ src/main/java/com/adriendr/dota2/dota2privatelearning/Dota2PrivateLearningApplication.java
@@ -0,0 +1,12 @@
+package com.adriendr.dota2.dota2privatelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2PrivateLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dota2PrivateLearningApplication.class, args);
+	}
+}
---
+++ src/test/java/com/adriendr/dota2/dota2privatelearning/Dota2PrivateLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adriendr.dota2.dota2privatelearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dota2PrivateLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
